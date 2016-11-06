8f8c95047d7e4d80fd69890fe116270039fcb2a6
zllzfw <zllzfw@yeah.net>
2016-11-06 17:15:29 +1100

    search similar pinyins with sqlite.

--- get_your_chinese_name/calc_similar_pinyin.py
+++ get_your_chinese_name/calc_similar_pinyin.py
@@ -1,37 +1,32 @@
 #coding: gbk
 import separate_vowel_consonant
-import consonant_diff
-import vowel_diff
-import xlrd
+import sqlite3
 
 
 def calc_sim_pinyin(input_pinyin):
-    cons_prop=[]
-    vowel_prop=[]
     similar_pinyin=[]
-    init_consonant, init_vowel = separate_vowel_consonant. \
-        separate_vowel_and_consonant(input_pinyin)
-#    print(init_consonant + '  ' + init_vowel)
+    std_pinyin=''
+    connection = sqlite3.connect('..\\resources\\database.db')
+    cursor=connection.execute(u'SELECT COUNT(std_pinyin) FROM 拼音整理1 WHERE pinyin=\'{0:s}\''.format(input_pinyin))
+    for row in cursor:
+        if row[0]!=0:
+            cursor1 = connection.execute(
+                u'SELECT std_pinyin FROM 拼音整理1 WHERE pinyin=\'{0:s}\''.format(input_pinyin))
+            for row1 in cursor1:
+                std_pinyin=row1[0]
+    init_consonant, init_vowel = separate_vowel_consonant.separate_vowel_and_consonant(std_pinyin)
+    cursor=connection.execute(u'SELECT 声母模糊表达表1.consonant2,韵母模糊表达表2.vowel2 \
+                              FROM 声母模糊表达表1 JOIN 韵母模糊表达表2 \
+                              WHERE 声母模糊表达表1.consonant1=\'{0:s}\'\
+                              AND 韵母模糊表达表2.vowel1=\'{1:s}\' \
+                              AND 声母模糊表达表1.VALUE+韵母模糊表达表2.VALUE<30\
+                              '.format(init_consonant,init_vowel))
+    for row in cursor:
+        cursor1=connection.execute(u'SELECT COUNT(std_pinyin) FROM 拼音整理1 WHERE std_pinyin=\'{0:s}\' '.format(row[0]+row[1]))
+        for row1 in cursor1:
+            if row1[0]!=0:
+                similar_pinyin.append(row[0]+row[1])
 
-    vowel_xls = xlrd.open_workbook('..\\resources\\韵母模糊表达表2.xls')  # 打开韵母模糊表达表
-    vowel_table = vowel_xls.sheets()[0]  # 打开sheet1
-    n_vowel_cols = vowel_table.ncols  # 获取列数
-
-    consonant_xls = xlrd.open_workbook('..\\resources\\声母模糊表达表1.xls')  # 打开声母模糊表达表
-    consonant_table = consonant_xls.sheets()[0]  # 打开sheet1
-    n_consonant_cols = consonant_table.ncols  # 获取列数
-
-    for i in range(n_consonant_cols):
-        cons_diff = consonant_diff.calc_2_consonant_diff(consonant_table.cell(i, 0).value, init_consonant)
-        if cons_diff<30:
-            cons_prop.append(consonant_table.cell(i,0).value)
-    for j in range(n_vowel_cols):
-        vowel_difference=vowel_diff.calc_2_vowel_diff(vowel_table.cell(j,0).value,init_vowel)
-        if vowel_difference<10:
-            vowel_prop.append(vowel_table.cell(j,0).value)
-    for cons in cons_prop:
-        for vowel in vowel_prop:
-            similar_pinyin.append(cons + vowel)
     return similar_pinyin
 
 if __name__ == "__main__":
--- get_your_chinese_name/xls_to_db.py
+++ get_your_chinese_name/xls_to_db.py
@@ -1,42 +1,117 @@
-#
+# coding=gbk
+# this program should run only  once
 import xlrd
 import sqlite3
 
 
-def turn_xls_to_db(xls_name, factor_name):
-    consonant_xls = xlrd.open_workbook('..\\resources\\' + xls_name + '.xls')  # ÊâìÂºÄÊãºÈü≥Ê®°Á≥äË°®ËææË°®
-    consonant_table = consonant_xls.sheets()[0]  # ÊâìÂºÄsheet1
-    n_rows = consonant_table.nrows  # Ëé∑ÂèñË°åÊï∞
-    n_cols = consonant_table.ncols  # Ëé∑ÂèñÂàóÊï∞
+def convert_xls_to_db(xls_name, factor_name):
+    """
+    to convert a comparing table
+    :param xls_name: the name of excel file
+    :param factor_name: the name of factors
+    :return: nothing
+    """
+    consonant_xls = xlrd.open_workbook('..\\resources\\' + xls_name + '.xls')  # ¥Úø™∆¥“Ùƒ£∫˝±Ì¥Ô±Ì
+    consonant_table = consonant_xls.sheets()[0]  # ¥Úø™sheet1
+    n_rows = consonant_table.nrows  # ªÒ»°–– ˝
+    n_cols = consonant_table.ncols  # ªÒ»°¡– ˝
 
-    connection = sqlite3.connect('..\\resources\\pinyin_diff.db')
-    connection.execute('''CREATE TABLE %s
-                             (ID INT PRIMARY KEY   NOT NULL,
-                              %s1 CHAR(5),
-                         %s2 CHAR(5),
-                      VALUE REAL);
-                ''' % (xls_name, factor_name, factor_name))
+    connection = sqlite3.connect('..\\resources\\database.db')
+    cursor = connection.execute(
+        u'SELECT COUNT(*) FROM sqlite_master WHERE  type=\'table\' AND name=\'{0:s}\''.format(xls_name))
+
+    for row in cursor:
+        if row[0] == 0:
+            connection.execute(u'CREATE TABLE {0:s}\
+                                (ID INT PRIMARY KEY   NOT NULL,\
+                                {1:s}1 CHAR(5),\
+                                {2:s}2 CHAR(5),\
+                                VALUE REAL)\
+                              '.format(xls_name, factor_name, factor_name))
     for i in range(1, n_rows):
         for j in range(1, n_cols):
-            if consonant_table.cell(i, j).value != '':
-
-                connection.execute(u'INSERT INTO {0:s}(ID,{1:s}1,{2:s}2,VALUE) VALUES({3:d},\"{4:s}\",\"{5:s}\",{6})'
-                                   .format(xls_name, factor_name, factor_name,
-                                           i * 100 + j,
-                                           consonant_table.cell(i, 0).value,
-                                           consonant_table.cell(0, j).value,
-                                           consonant_table.cell(i, j).value))
-                connection.commit()
-                print(u'INSERT INTO {0:s}(ID,{1:s}1,{2:s}2,VALUE) VALUES({3:d},\"{4:s}\",\"{5:s}\",{6})'
-                      .format(xls_name, factor_name, factor_name,
-                              i * 100 + j,
-                              consonant_table.cell(i, 0).value,
-                              consonant_table.cell(0, j).value,
-                              consonant_table.cell(i, j).value))
+            cursor = connection.execute(
+                u'SELECT COUNT(*) FROM {0:s} WHERE  {1:s}1=\'{2:s}\' AND {3:s}2=\'{4:s}\''
+                    .format(xls_name, factor_name, consonant_table.cell(i, 0).value,
+                            factor_name, consonant_table.cell(0, j).value))
+            for row in cursor:
+                if row[0] == 0:
+                    if consonant_table.cell(i, j).value == '':
+                        connection.execute(
+                            u'INSERT INTO {0:s}(ID,{1:s}1,{2:s}2,VALUE) VALUES({3:d},\"{4:s}\",\"{5:s}\",{6})'
+                                .format(xls_name, factor_name, factor_name,
+                                        i * 100 + j,
+                                        consonant_table.cell(i, 0).value,
+                                        consonant_table.cell(0, j).value,
+                                        consonant_table.cell(j, i).value))
+                        connection.commit()
+                        print(u'INSERT INTO {0:s}(ID,{1:s}1,{2:s}2,VALUE) VALUES({3:d},\"{4:s}\",\"{5:s}\",{6})'
+                              .format(xls_name, factor_name, factor_name,
+                                      i * 100 + j,
+                                      consonant_table.cell(i, 0).value,
+                                      consonant_table.cell(0, j).value,
+                                      consonant_table.cell(j, i).value))
+                    else:
+                        connection.execute(u'INSERT INTO {0:s}(ID,{1:s}1,{2:s}2,VALUE) VALUES({3:d},\"{4:s}\",'
+                                           u'\"{5:s}\",{6})'
+                                           .format(xls_name, factor_name, factor_name,
+                                                   i * 100 + j,
+                                                   consonant_table.cell(i, 0).value,
+                                                   consonant_table.cell(0, j).value,
+                                                   consonant_table.cell(i, j).value))
+                        connection.commit()
+                        print(u'INSERT INTO {0:s}(ID,{1:s}1,{2:s}2,VALUE) VALUES({3:d},\"{4:s}\",\"{5:s}\",{6})'
+                              .format(xls_name, factor_name, factor_name,
+                                      i * 100 + j,
+                                      consonant_table.cell(i, 0).value,
+                                      consonant_table.cell(0, j).value,
+                                      consonant_table.cell(i, j).value))
     return
 
 
-if __name__ == "__main__":
-    turn_xls_to_db('Â£∞ÊØçÊ®°Á≥äË°®ËææË°®1', 'consonant')
-    turn_xls_to_db('ÈüµÊØçÊ®°Á≥äË°®ËææË°®2', 'consonant')
+def convert_xls_to_db_2(xls_name, factor_name1, factor_name2):
+    """
+    to convert one to one xls tables
+    :param xls_name: string, the name of xls files
+    :param factor_name1: first factor's name
+    :param factor_name2: second factor's name
+    :return: nothing
+    """
+    consonant_xls = xlrd.open_workbook('..\\resources\\' + xls_name + '.xls')  # ¥Úø™excel±Ì
+    consonant_table = consonant_xls.sheets()[0]  # ¥Úø™sheet1
+    n_rows = consonant_table.nrows  # ªÒ»°–– ˝
 
+    connection = sqlite3.connect('..\\resources\\database.db')
+    cursor = connection.execute(
+        u'SELECT COUNT(*) FROM sqlite_master WHERE  type=\'table\' AND name=\'{0:s}\''.format(xls_name))
+    for row in cursor:
+        if row[0] == 0:
+            connection.execute(u'CREATE TABLE {0:s}(ID INT PRIMARY KEY   NOT NULL,\
+                                {1:s} CHAR(10),\
+                                {2:s} CHAR(10))'.format(xls_name, factor_name1, factor_name2))
+
+    for i in range(1, n_rows):
+        cursor = connection.execute(
+            u'SELECT COUNT(*) FROM {0:s} WHERE  {1:s}=\'{2:s}\''
+                .format(xls_name, factor_name1, consonant_table.cell(i, 0).value))
+        for row in cursor:
+            if row[0] == 0 and consonant_table.cell(i, 0).value != '':
+                connection.execute(
+                    u'INSERT INTO {0:s}(ID,{1:s},{2:s}) VALUES({3:d},\"{4:s}\",\"{5:s}\")'
+                        .format(xls_name, factor_name1, factor_name2,
+                                i, consonant_table.cell(i, 0).value,
+                                consonant_table.cell(i, 1).value))
+                connection.commit()
+                print(
+                    u'INSERT INTO {0:s}(ID,{1:s},{2:s}) VALUES({3:d},\"{4:s}\",\"{5:s}\")'
+                        .format(xls_name, factor_name1, factor_name2,
+                                i, consonant_table.cell(i, 0).value,
+                                consonant_table.cell(i, 1).value))
+    return
+
+
+# test
+if __name__ == "__main__":
+    convert_xls_to_db('…˘ƒ∏ƒ£∫˝±Ì¥Ô±Ì1', 'consonant')
+    convert_xls_to_db('‘œƒ∏ƒ£∫˝±Ì¥Ô±Ì2', 'vowel')
+    convert_xls_to_db_2('∆¥“Ù’˚¿Ì1', 'pinyin', 'std_pinyin')
